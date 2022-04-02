592c9efaf7a83546a357aa00dd616c2613270fdf
root <19MansH@gordons.school>
2022-04-02 15:14:31 +0100

    add leave command

--- cogs/voiceCog.py
+++ cogs/voiceCog.py
@@ -23,6 +23,20 @@ class VoiceCog(commands.Cog):
         if ctx.user.voice:
             channel = ctx.user.voice.channel
             await channel.connect()
+            await ctx.response.send_message('connected successfully!!!')
             return
 
         await ctx.response.send_message('you need to be in a vc for this to work')
+
+    @app_commands.command(description='leaves a voice channel')
+    async def leave(self, ctx: Interaction):
+        try:
+            await ctx.guild.voice_client.disconnect(force=True)
+            await ctx.response.send_message('left vc')
+
+        except Exception as er:
+            if isinstance(er, AttributeError):
+                await ctx.response.send_message('I am not in a voice channel')
+
+            else:
+                await ctx.response.send_message(f'{er}')
