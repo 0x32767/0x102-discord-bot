85b7e694b8aa70e2f90c4f91acff0fa1c9d6f68c
badUsername123456 <95779166+0x32767@users.noreply.github.com>
2022-07-20 18:50:51 +0100

    made minor changes
    these are minor changes

--- c++/test.py
+++ c++/test.py
@@ -4,4 +4,4 @@ from ctypes import cdll, c_char
 lib = cdll.LoadLibrary(".\\builds\\profanity.so")
 
 
-print(type(lib.isCloseMach("hello", "hello")))
+print(lib.isCloseMach("hello", ""))
--- cogs/_botSync.py
+++ cogs/_botSync.py
@@ -1,10 +1,23 @@
+from configparser import ConfigParser
 from aiohttp import ClientSession
 from aiosqlite import connect
 
 
 class botSync:
     def __init__(self: "botSync", file: str) -> None:
+        self.session: ClientSession = ClientSession()
+        self.conf: ConfigParser = ConfigParser()
+        self.conf.read("vars.ini")
         self.file: str = file
 
-    def syncLocal(self: "botSync") -> None:
-        ...
+    async def syncLocal(self: "botSync") -> None:
+        async with connect(self.file) as db:
+            async with db.cursor() as curr:
+                req = await self.self.session.get(
+                    f"{self.conf['api']['href']}/updates/"
+                )
+                data = await req.json()
+                del req
+
+                for gId in data:
+                    req = await self.session.get(f"{self.conf['api']['href']}/items/")
