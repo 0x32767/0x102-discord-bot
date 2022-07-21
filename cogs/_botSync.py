d4d9cba24c108604f06a053071e27850583626c0
badUsername123456 <95779166+0x32767@users.noreply.github.com>
2022-07-21 21:46:11 +0100

    Update _botSync.py

--- cogs/_botSync.py
+++ cogs/_botSync.py
@@ -1,6 +1,7 @@
 from configparser import ConfigParser
-from aiohttp import ClientSession
 from aiosqlite import connect
+from aiohttp import ClientSession
+from json import loads
 
 
 class botSync:
@@ -10,14 +11,62 @@ class botSync:
         self.conf.read("vars.ini")
         self.file: str = file
 
-    async def syncLocal(self: "botSync") -> None:
-        async with connect(self.file) as db:
-            async with db.cursor() as curr:
-                req = await self.self.session.get(
-                    f"{self.conf['api']['href']}/updates/"
-                )
-                data = await req.json()
-                del req
+    async def getCloud(self: "botSync") -> str:
+        """
+        | This function will get the guilds with the changed settings (for
+        | optimization purposes) and will then iterate over all said guilds
+        | and edit them in a local database.
+        |
+        | This function should
+        """
+        async with self.session.get(f"{self.baseUrl}edited/") as response:
+            txt = await response.text()
+            guilds: list[int] = loads(txt)
+            # this is a list of guild ids [1, 2, 3, 4, 5]
 
-                for gId in data:
-                    req = await self.session.get(f"{self.conf['api']['href']}/items/")
+            del txt
+
+        async with connect(self.file) as conn:
+            async with conn.curr() as curr:
+                for guildId in guilds:
+                    async with self.session.get(f"{self.baseUrl}items/{guildId}/") as res:
+                        """
+                        | The response from: http://XX.XXX.XXX.XX/items/{guildId}/
+                        | would return a json object that looks like this:
+                        |
+                        | {
+                        |  "id": 0,
+                        |  "use_whitelisting": false,
+                        |  "use_blacklisting": false,
+                        |  "allow_links": false,
+                        |  "spam_mod": false,
+                        |  "auto_mod": false,
+                        |  "logging": false,
+                        |  "run_cc": false,
+                        |  "reputation": false
+                        | }
+                        """
+                        txt = await res.text()
+                        guild: dict = loads(txt)
+                        del txt
+
+                        curr.execute(f"""
+                            INSERT INTO settings VALUES(
+                                ?, ?, ?, ?, ?, ?, ?, ?
+                            );
+                            """, (
+                                guildId,
+                                int(guild["use_whitelisting"]),
+                                int(guild["use_blacklisting"]),
+                                int(guild["allow_links"]),
+                                int(guild["spam_mod"]),
+                                int(guild["auto_mod"]),
+                                int(guild["logging"]),
+                                int(guild["run_cc"]),
+                                int(guild["reputation"])
+                            )
+                        )
+
+    @property
+    def baseUrl(self: "botSync") -> str:
+        return self.conf.get("api", "url")
