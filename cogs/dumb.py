f9d61344cfa41be07f4293e2e7a5bef100c9411a
badUsername123456 <95779166+0x32767@users.noreply.github.com>
2022-08-10 19:32:01 +0100

    added rickroll and hack command
    I have also updated the license to GeoffreyWesthoff's wants which is to give him credit for his work.

--- cogs/dumb.py
+++ cogs/dumb.py
@@ -1,10 +1,45 @@
-from typing import Generator
-from discord import Interaction, app_commands, Object, Message, User
+"""
+
+Copyright <10/08/2022> Han P.B Manseck
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject
+to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+Credits:
+ - https://github.com/GeoffreyWesthoff/Dank-Memer
+
+Credit goes to GeoffreyWesthoff for the inspiration for most of the
+commands.
+"""
+
+from discord import Interaction, app_commands, Object, Message, User, Embed
 from usefull.textDecorations import progressBar
 from hashlib import sha256, sha1, md5
 from discord.ext import commands
 import cogs._helpCommandSetup
+from typing import Generator
 from cache import cacheGet
+from json import load
+import asyncio
+import random
 
 
 async def setup(bot: commands.Bot) -> None:
@@ -26,8 +61,29 @@ class DumbCommandsCog(commands.Cog):
     @app_commands.command(description="get a users discord token")
     @app_commands.describe(user="who's token you want to see")
     async def hack(self: "DumbCommandsCog", ctx: Interaction, user: User) -> None:
-        dec: Generator[str] = progressBar()
+        with open("D:\\programing\\0x102-discord-bot\\assets\\hackMessages.json", "r") as f:
+            messages: dict[str, list[str]] = load(f)
+
+        dec: Generator[str] = progressBar(messages["updates"], messages["errors"])
         msg: Message = await ctx.response.send_message()
+
+        for msg in dec:
+            await msg.edit(content=msg)
+            await asyncio.sleep(0.1)
+        msg.edit(content=f"{user.mention} has a token of `{await self._dummyHash(user)}`")
+
+    @cogs._helpCommandSetup.record()
+    @app_commands.command(description="click the link")
+    async def rr(self: "DumbCommandsCog", ctx: Interaction) -> Embed:
+        return await ctx.response.send_message(embed=await self._getRickRoll())
 
     async def _dummyHash(self: "DumbCommandsCog", user: User) -> str:
         return f'{sha256(user.id.to_bytes(8, "big")).hexdigest()}.{sha1(user.id.to_bytes(8, "big")).hexdigest()}.{md5(user.id.to_bytes(8, "big")).hexdigest()}'
+
+    async def _getRickRoll(self: "DumbCommandsCog") -> Embed:
+        with open("D:\\programing\\0x102-discord-bot\\assets\\rickRolls.json", "r") as f:
+            rickRolls: list[dict[str, str]] = load(f)
+
+        rrInfo: dict[str, str] = random.choice(rickRolls)
+
+        return Embed(title=rrInfo["Excuse"], url=rrInfo["Link"])
